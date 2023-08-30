7a1f6e0d630d1c18ebb7bf664009fcebf47d5fe9
abbeyrussell <abbeyrussell@localhost>
2023-08-30 13:32:38 -0500

    yay!

--- src/studio0/Message.java
+++ src/studio0/Message.java
@@ -3,6 +3,6 @@ package studio0;
 public class Message {
 
 	public static void main(String args[]) {
-		System.out.println("I want to take a nap.");
+		System.out.println("Thank you for being a great group mate!");
 	}
 }
